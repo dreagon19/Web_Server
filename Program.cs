f767ccd2661ae37e6738ef0dbe1cb842f713da27
Jitesh Kumar <60359722+dreagon19@users.noreply.github.com>
2022-11-15 00:47:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Server;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Server
+{
+    /// <summary>
+    /// Since the webserver is stateless so most of the behavior can be implemented as static singelton.
+    /// </summary>
+    public static class Server
+    {
+        private static HttpListener _listener;
+
+        
+
+
+    }
+}
