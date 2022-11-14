07bf7877e211a8ca71bf628e1afe8da2fb49b03b
Jitesh Kumar <60359722+dreagon19@users.noreply.github.com>
2022-11-15 01:11:38 +0530

    Feature/ Getting local-host IP address

--- Server.cs
+++ Server.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,15 @@ namespace Web_Server
     {
         private static HttpListener _listener;
 
-        
+        //Returning the List of assigned IP to localHost network devices includes wireless or wired
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry hostEntry;
+            hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> result = hostEntry.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+            return result;
+        }
 
 
     }
